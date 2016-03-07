0d3e13f0e5b16c2a2a8799e4bbe88b7a8216f666
juran08 <juran.08@hotmail.com>
2016-03-07 08:57:10 +0100

    Update Program.cs

--- NewProject/NewProject/Program.cs
+++ NewProject/NewProject/Program.cs
@@ -11,6 +11,7 @@ namespace NewProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("MARIN JE: ");
         }
     }
 }
